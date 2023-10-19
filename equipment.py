1a0e8a5747b1f257857552d8e88ed6ee02e5e316
PointyBytes <pointy.bytes@gmail.com>
2023-10-19 20:16:52 +0200

    Added another improvised weapon

--- equipment.py
+++ equipment.py
@@ -3,7 +3,15 @@ improvised_weapons = {
         "activation_icon": "Some_Symbol",
         "speed": 1,
         "accuracy": 7,
-        "strength": "Some_Value",  # To be determined
+        "strength": "Some_Value",
+        "keywords": ["bludgeoning", "fragile"],
+    },
+    "Snapped Bone": {
+        "activation_icon": "Another_Symbol",
+        "speed": 2,  # Or perhaps faster as you said
+        "accuracy": 8,
+        "strength": "Another_Value",
+        "keywords": ["piercing", "fragile"],
     }
     # Additional weapons can be added here
 }
