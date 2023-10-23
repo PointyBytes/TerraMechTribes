1edb3f73a6a9ee227fb7e2e27c115d9b8b73eaec
PointyBytes <pointy.bytes@gmail.com>
2023-10-23 16:13:27 +0200

    Updated comments and TODO

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
-from board import Board
-from entity import Survivor
-from equipment import Equipment
-from upkeep_phase import UpkeepPhase
-from combat import CombatPhase, ActionManager
-from player import Player
-from quarry import Quarry
+from board import Board  # Importing the Board class to represent the game board
+from entity import Survivor  # For representing survivor entities in the game
+from equipment import Equipment  # Handling equipment logic
+from upkeep_phase import UpkeepPhase  # Managing the Upkeep Phase
+from combat import CombatPhase, ActionManager  # Managing the Combat Phase
+from player import Player  # Representing each player in the game
+from quarry import Quarry  # For representing quarry (monsters) in the game
 
 
 def main():
     # Initialize the game state
-    players = [Player() for _ in range(4)]  # Example: Create 4 players
+
+    # Create 4 example players.
+    # TODO: Allow dynamic player count based on user input or game settings.
+    players = [Player() for _ in range(4)]
+
+    # Initialize game board.
+    # TODO: Customize board layout or dimensions.
     board = Board()
+
+    # Initialize Upkeep Phase logic.
     upkeep_phase = UpkeepPhase()
+
+    # Initialize Combat Phase logic and Action Manager.
     combat_phase = CombatPhase()
     action_manager = ActionManager()
 
-    quarry_entities = []  # Populate this list as needed
+    # List to hold quarry (monster) entities.
+    # TODO: Populate this list based on game conditions.
+    quarry_entities = []
+
+    # Flag to indicate if the game is over.
     game_over = False
 
-    while not game_over:  # Main game loop
-        # Example: Upkeep Phase
+    # Main game loop
+    while not game_over:
+        # Upkeep Phase: Run upkeep logic for each player.
         for player in players:
             upkeep_phase.run(player)
 
-        # Example: Combat Phase
+        # Combat Phase: Execute movement and general actions for each player.
         for player in players:
             action_manager.movement_action(player, board)
-            action_manager.general_action(
-                player, board, quarry_entities
-            )  # Example: Add quarry_entities as needed
 
-        # Example: Check game over conditions
+            # TODO: Add quarry_entities as needed.
+            action_manager.general_action(player, board, quarry_entities)
+
+        # Check for game over conditions.
+        # TODO: Implement check_game_over_conditions function.
         # game_over = check_game_over_conditions()
 
-    # Example: End-of-game logic here
+    # End-of-game logic.
+    # TODO: Summarize game results, announce winner, etc.
     print("Game Over")
 
 
