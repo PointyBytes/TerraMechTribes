f4c4354e2f591b302b9ee7f515bb97387f462c15
PointyBytes <pointy.bytes@gmail.com>
2023-10-24 19:25:49 +0200

    Light update

--- hunt.py
+++ hunt.py
@@ -1,5 +1,15 @@
+import random  # Import the random module
+
+
 class HuntingPhase:
-    def __init__(self):
+    biotope_general_hunt_cards = {
+        "forest": ["Lost in the Woods", "Bear Trap", "Mysterious Clearing"],
+        "desert": ["Sandstorm", "Oasis Mirage", "Scorpion Attack"],
+        # ... more biotopes
+    }
+
+    def __init__(self, current_biotope):
+        self.current_biotope = current_biotope
         self.general_event_deck = []  # General events that can happen during the hunt
         self.specific_event_deck = {}  # Events specific to the quarry being hunted
         self.distance_from_home = 0  # How far the players have traveled from home
@@ -23,5 +33,19 @@ class HuntingPhase:
         """Check if the players have encountered the quarry."""
         # TODO: Implement logic to determine if an encounter occurs
         pass
+
+    def resolve_hunt_track(
+        self, quarry
+    ):  # Changed from a standalone function to a method
+        for card_type in quarry.hunt_track:  # Changed from salamander_hunt_track
+            if card_type == "general":
+                card = random.choice(
+                    HuntingPhase.biotope_general_hunt_cards[self.current_biotope]
+                )
+            elif card_type == "specific":
+                card = random.choice(
+                    quarry.specific_hunt_cards
+                )  # Changed from salamander_specific_cards
+            # ... resolve the card
 
     # TODO: Add more methods for other hunting phase activities like resource gathering, etc.
--- quarry.py
+++ quarry.py
@@ -9,12 +9,28 @@ class Quarry:
 
 # Salamander class inherits from Quarry and represents the specific behaviors and attributes of a Salamander.
 class Salamander(Quarry):
+    common_hunt_cards = [
+        "Easy Track",
+        "Ambush",
+        "Water Hazard",
+        "Deadly Trap",
+    ]  # common to all ages
+    hunt_track = [  # Changed from salamander_hunt_track to make it more general
+        "general",
+        "specific",  # Changed from "salamander" to "specific"
+        "specific",  # Changed from "salamander" to "specific"
+        "general",
+        "boss",  # Changed from "salamander_boss" to "boss"
+    ]  # Just an example
+
     def __init__(self, name, difficulty, age):
         super().__init__(name, difficulty)
         self.age = age  # Age can be "young", "adult", or "elder"
 
-        # Initialize AI deck based on Salamander's age
+        # Initialize AI deck and hunt cards based on Salamander's age
         self.initialize_ai_deck()
+        self.specific_hunt_cards = []  # Initialize the specific hunt cards
+        self.initialize_hunt_cards()
 
         # Example terrain setup
         self.terrain_setup = [
@@ -22,6 +38,14 @@ class Salamander(Quarry):
             {"type": "shoreline", "position": (2, 0)},
             # TODO: Add more terrain types and their positions.
         ]
+
+    def set_encounter_distance(self):
+        if self.age == "young":
+            self.encounter_distance = 3
+        elif self.age == "adult":
+            self.encounter_distance = 5
+        else:  # elder
+            self.encounter_distance = 7
 
     def initialize_ai_deck(self):
         """Initialize the AI deck based on the Salamander's age."""
@@ -32,6 +56,10 @@ class Salamander(Quarry):
             self.ai_deck = base_attacks + ["Slime", "Death Roll"]
         else:  # elder
             self.ai_deck = base_attacks + ["Slime", "Death Roll", "Whirlpool"]
+
+    def initialize_hunt_cards(self):
+        """Initialize the specific hunt cards based on the Salamander's age."""
+        # TODO: Implement logic for initializing hunt cards based on age
 
     # TODO: Implement Salamander-specific actions like "Death Roll", "Whirlpool", etc.
     def death_roll(self, target):
