a208dc7232445d3e1aea4b69a1046fe74c5183bc
PointyBytes <pointy.bytes@gmail.com>
2023-10-24 17:59:29 +0200

    Removed zone.py and included it in hunt.py

--- hunt.py
+++ hunt.py
@@ -1,3 +1,27 @@
 class HuntingPhase:
     def __init__(self):
+        self.general_event_deck = []  # General events that can happen during the hunt
+        self.specific_event_deck = {}  # Events specific to the quarry being hunted
+        self.distance_from_home = 0  # How far the players have traveled from home
+
+    def draw_general_event(self):
+        """Draw a card from the general event deck based on the distance from home."""
+        # TODO: Implement the logic to draw an event card based on the distance
         pass
+
+    def draw_specific_event(self, quarry):
+        """Draw a card from the specific event deck for the quarry."""
+        # TODO: Implement the logic to draw a quarry-specific event card
+        pass
+
+    def move_quarry(self, quarry):
+        """Move the quarry closer or further away from the players."""
+        # TODO: Implement logic for quarry movement
+        pass
+
+    def check_encounter(self, quarry):
+        """Check if the players have encountered the quarry."""
+        # TODO: Implement logic to determine if an encounter occurs
+        pass
+
+    # TODO: Add more methods for other hunting phase activities like resource gathering, etc.
--- zone.py
+++
@@ -1,4 +0,0 @@
-class Zone:
-    def __init__(self, name, discovery_level):
-        self.name = name
-        self.discovery_level = discovery_level
